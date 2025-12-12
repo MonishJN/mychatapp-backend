52a23093c202fc976481195637fa5bf1ed4c56ad
MonishJN <jnmonish@gmail.com>
2025-12-12 16:20:24 +0530

    cors fix no.x42

--- app.js
+++ app.js
@@ -10,38 +10,33 @@ import notificationRoutes from "./routes/notificationRoutes.js";
 
 const app = express();
 
-// export const allowedOrigins = [
-//   "https://mychatapp-two.vercel.app",
-//   "https://mychatapp-git-main-monishjns-projects.vercel.app",
-//   "https://mychatapp-9c736ojtx-monishjns-projects.vercel.app",
-//   // Add your local port here, which the Vercel app will be connecting to
-//   "http://localhost:5000", 
-//   "http://localhost:4000" // Keep this if you also run your frontend locally
-// ];
-
-// app.use(cors({
-//   origin: function (origin, callback) {
-//      console.log("CORS CHECK:", origin); 
-//     if (!origin) return callback(null, true); // mobile apps, curl, etc.
-
-//     if (
-//       allowedOrigins.includes(origin) ||
-//       /^https:\/\/mychatapp-.*\.vercel\.app$/.test(origin)  // Allow all Vercel previews
-//     ) {
-//       callback(null, true);
-//     } else {
-//       console.log("HTTP CORS BLOCKED:", origin);
-//       callback(new Error("Not allowed by CORS"));
-//     }
-//   },
-//   credentials: true
-// }));
+export const allowedOrigins = [
+  "https://mychatapp-two.vercel.app",
+  "https://mychatapp-git-main-monishjns-projects.vercel.app",
+  "https://mychatapp-9c736ojtx-monishjns-projects.vercel.app",
+  // Add your local port here, which the Vercel app will be connecting to
+  "http://localhost:5000", 
+  "http://localhost:4000" // Keep this if you also run your frontend locally
+];
 
 app.use(cors({
-  origin: '*',
-  methods: ["GET", "POST", "OPTIONS"], 
+  origin: function (origin, callback) {
+     console.log("CORS CHECK:", origin); 
+    if (!origin) return callback(null, true); // mobile apps, curl, etc.
+
+    if (
+      allowedOrigins.includes(origin) ||
+      /^https:\/\/mychatapp-.*\.vercel\.app$/.test(origin)  // Allow all Vercel previews
+    ) {
+      callback(null, true);
+    } else {
+      console.log("HTTP CORS BLOCKED:", origin);
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true
 }));
+
 
 
 app.use(express.json());
