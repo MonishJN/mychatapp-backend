3b5326d1a245bead7260d8e11d719222b4b0d176
MonishJN <jnmonish@gmail.com>
2025-12-13 12:49:44 +0530

    lets hope it works

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 import express from "express";
 import cors from "cors";
-import authRoutes from "./routes/authRoutes.js";
-import friendRoutes from "./routes/friendRoutes.js";
-import searchRoutes from "./routes/searchRoutes.js";
-import profileRoutes from "./routes/profileRoutes.js";
-import chatRoutes from "./routes/chatRoutes.js";
-import notificationRoutes from "./routes/notificationRoutes.js";
+// import authRoutes from "./routes/authRoutes.js";
+// import friendRoutes from "./routes/friendRoutes.js";
+// import searchRoutes from "./routes/searchRoutes.js";
+// import profileRoutes from "./routes/profileRoutes.js";
+// import chatRoutes from "./routes/chatRoutes.js";
+// import notificationRoutes from "./routes/notificationRoutes.js";
 
 
 const app = express();
@@ -43,12 +43,12 @@ app.use(express.json());
 
 
 // Routes
-app.use("/api",authRoutes);
-app.use("/api/friends",friendRoutes);
-app.use("/api",searchRoutes);
-app.use("/api",profileRoutes);
-app.use("/api/chat",chatRoutes);
-app.use("/api/notifications",notificationRoutes);
+// app.use("/api",authRoutes);
+// app.use("/api/friends",friendRoutes);
+// app.use("/api",searchRoutes);
+// app.use("/api",profileRoutes);
+// app.use("/api/chat",chatRoutes);
+// app.use("/api/notifications",notificationRoutes);
 
 
 export default app;
