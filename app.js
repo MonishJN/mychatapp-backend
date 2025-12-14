2728d1d1e016825faed82ddb07faa681c723e132
MonishJN <jnmonish@gmail.com>
2025-12-14 19:59:52 +0530

    cors issue

--- app.js
+++ app.js
@@ -10,11 +10,31 @@ import notificationRoutes from "./routes/notificationRoutes.js";
 
 const app = express();
 
-app.use(cors({
-  origin: "https://mychatapp-two.vercel.app",
-  methods: ["GET","POST"],
-  credentials: true
-}));
+// app.use(cors({
+//   origin: "https://mychatapp-two.vercel.app",
+//   methods: ["GET","POST"],
+//   credentials: true
+// }));
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin (Postman, curl)
+      if (!origin) return callback(null, true);
+
+      if (origin === "https://mychatapp-two.vercel.app") {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
+app.options(/.*/, cors());
 
 
 
--- config/db.js
+++ config/db.js
@@ -5,20 +5,20 @@ dotenv.config();
 import mysql from 'mysql2/promise';
 
 // localhost setup
-// const pool = mysql.createPool({
-//   host: process.env.db_host,
-//   user: process.env.db_user,
-//   password: process.env.db_password,
-//   database: process.env.db_name,
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0
-// });
+const pool = mysql.createPool({
+  host: process.env.db_host,
+  user: process.env.db_user,
+  password: process.env.db_password,
+  database: process.env.db_name,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
 
 
 
 //railway database setup
-const pool = mysql.createPool(process.env.DATABASE_URL);
+// const pool = mysql.createPool(process.env.DATABASE_URL);
 
 
 export default pool;
