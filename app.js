74c80f50628b8c6bc62b205cf53dd0a8df3efc39
MonishJN <jnmonish@gmail.com>
2025-12-16 16:37:06 +0530

    switch to render

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
 import express from "express";
 import cors from "cors";
-// import authRoutes from "./routes/authRoutes.js";
-// import friendRoutes from "./routes/friendRoutes.js";
-// import searchRoutes from "./routes/searchRoutes.js";
-// import profileRoutes from "./routes/profileRoutes.js";
-// import chatRoutes from "./routes/chatRoutes.js";
-// import notificationRoutes from "./routes/notificationRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+import friendRoutes from "./routes/friendRoutes.js";
+import searchRoutes from "./routes/searchRoutes.js";
+import profileRoutes from "./routes/profileRoutes.js";
+import chatRoutes from "./routes/chatRoutes.js";
+import notificationRoutes from "./routes/notificationRoutes.js";
 
 
 const app = express();
 
-// app.use(cors({
-//   origin: "https://mychatapp-two.vercel.app",
-//   methods: ["GET","POST"],
-//   credentials: true
-// }));
+
 
 app.use(
   cors({
@@ -45,12 +41,12 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.status(200).send('Server Healthy');
 });
-// app.use("/api",authRoutes);
-// app.use("/api/friends",friendRoutes);
-// app.use("/api",searchRoutes);
-// app.use("/api",profileRoutes);
-// app.use("/api/chat",chatRoutes);
-// app.use("/api/notifications",notificationRoutes);
+app.use("/api",authRoutes);
+app.use("/api/friends",friendRoutes);
+app.use("/api",searchRoutes);
+app.use("/api",profileRoutes);
+app.use("/api/chat",chatRoutes);
+app.use("/api/notifications",notificationRoutes);
 
 
 export default app;
--- server.js
+++ server.js
@@ -3,8 +3,8 @@ dotenv.config();
 
 import { createServer } from "http";
 import app from "./app.js";
-// import { initWebSocket,startQueueProcessor } from "./websocket/chatSocket.js";
-// import { createClient } from "redis";
+import { initWebSocket,startQueueProcessor } from "./websocket/chatSocket.js";
+import { createClient } from "redis";
 
 const PORT = process.env.PORT;
 if (!PORT) {
@@ -13,45 +13,41 @@ if (!PORT) {
 
 const server = createServer(app);
 
-// server.listen(PORT,"0.0.0.0", () => {
-//   console.log(`🚀 Server running on ${PORT}`);
-// });
-
 server.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on ${PORT}`);
 });
 
 // Init WebSocket
-// initWebSocket(server);
+initWebSocket(server);
 
 // Declare Redis clients
-// export const redisQueue = createClient({ url: process.env.REDIS_URL });
-// export const redis = createClient({ url: process.env.REDIS_URL });
+export const redisQueue = createClient({ url: process.env.REDIS_URL });
+export const redis = createClient({ url: process.env.REDIS_URL });
 
-// // Async init function to connect them
-// async function initRedis() {
-//   try {
-//     redisQueue.on("error", (err) =>
-//       console.error("RedisQueue Client Error", err)
-//     );
-//     redis.on("error", (err) =>
-//       console.error("Redis Client Error", err)
-//     );
+// Async init function to connect them
+async function initRedis() {
+  try {
+    redisQueue.on("error", (err) =>
+      console.error("RedisQueue Client Error", err)
+    );
+    redis.on("error", (err) =>
+      console.error("Redis Client Error", err)
+    );
 
-//     await Promise.all([
-//       redisQueue.connect(),
-//       redis.connect()
-//     ]);
+    await Promise.all([
+      redisQueue.connect(),
+      redis.connect()
+    ]);
 
-//     startQueueProcessor(redisQueue);
-//     console.log("✅ Redis connected");
-//   }catch(err){
-//     console.error("Redis connection error:", err);
-//   }
-// }
+    startQueueProcessor(redisQueue);
+    console.log("✅ Redis connected");
+  }catch(err){
+    console.error("Redis connection error:", err);
+  }
+}
 
-// // Kick off Redis init
-// initRedis();
+// Kick off Redis init
+initRedis();
 
 
 
